6062c7ab69b5c4ffeae8510016548c135700f926
120356aa <yurlovandrew@gmail.com>
2019-04-12 20:16:32 -0700

    dashbaord button render and fire test

--- src/components/dashboard.test.js
+++ src/components/dashboard.test.js
@@ -7,4 +7,23 @@ describe('Dashboard component', () => {
   it('Renders without crashing', () => {
     render(<Dashboard />);
   });
+
+  it('render every button', () => {
+    const dashboard = render(<Dashboard />);
+
+    const strikeBTN = dashboard.getByTestId('strikeBTN');
+    const ballBTN = dashboard.getByTestId('ballBTN');
+    const foulBTN = dashboard.getByTestId('foulBTN');
+    const hitBTN = dashboard.getByTestId('hitBTN');
+
+    fireEvent.click(strikeBTN);
+    fireEvent.click(ballBTN);
+    fireEvent.click(foulBTN);
+    fireEvent.click(hitBTN);
+
+    expect(strikeBTN).toHaveTextContent(/strike/i);
+    expect(ballBTN).toHaveTextContent(/ball/i);
+    expect(foulBTN).toHaveTextContent(/foul/i);
+    expect(hitBTN).toHaveTextContent(/hit/i);
+  });
 });
